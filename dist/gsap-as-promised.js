31430fdc2fe3b2c028df2c7218acf1c370834a06
danielkalen <d@danielkalen.com>
2017-07-16 18:10:04 -0400

    ""

--- dist/gsap-as-promised.js
+++ dist/gsap-as-promised.js
@@ -1943,44 +1943,6 @@ var process = require(2);
 }).call(this);
 return module.exports;
 },
-0: function (require, module, exports) {
-function wrapLib(gsap){
-	function wrapper(a,b,c){
-		return wrapper.to(a,b,c);
-	}
-
-	wrapper.all = function(target){
-		return Promise.all(target)
-	};
-
-	Object.keys(gsap).forEach(function(method){
-		wrapper[method] = function(a,b,c,d){
-			var tween, promise, opts = arguments[arguments.length-1];
-			
-			promise = new Promise(function(resolve){
-				opts.onComplete = resolve
-			});
-			
-			tween = gsap[method](a,b,c,d);
-			
-			tween.promise = promise;
-			tween.then = function(next, reject){
-				promise.then(next, reject);
-			}
-
-			return tween;
-		}
-	});
-
-	return wrapper;
-}
-
-var wrapped = wrapLib(require(1));
-wrapped.wrap = wrapLib;
-
-module.exports = wrapped;;
-return module.exports;
-},
 2: function (require, module, exports) {
 // shim for using process in browser
 var process = module.exports = {};
@@ -2167,6 +2129,46 @@ process.chdir = function (dir) {
 };
 process.umask = function() { return 0; };
 ;
+return module.exports;
+},
+0: function (require, module, exports) {
+function wrapLib(gsap){
+	function wrapper(a,b,c){
+		return wrapper.to(a,b,c);
+	}
+
+	wrapper.lib = gsap;
+
+	wrapper.all = function(target){
+		return Promise.all(target)
+	};
+
+	Object.keys(gsap).forEach(function(method){
+		wrapper[method] = function(a,b,c,d){
+			var tween, promise, opts = arguments[arguments.length-1];
+			
+			promise = new Promise(function(resolve){
+				opts.onComplete = resolve
+			});
+			
+			tween = gsap[method](a,b,c,d);
+			
+			tween.promise = promise;
+			tween.then = function(next, reject){
+				promise.then(next, reject);
+			}
+
+			return tween;
+		}
+	});
+
+	return wrapper;
+}
+
+var wrapped = wrapLib(require(1));
+wrapped.wrap = wrapLib;
+
+module.exports = wrapped;;
 return module.exports;
 }
 }, this);
